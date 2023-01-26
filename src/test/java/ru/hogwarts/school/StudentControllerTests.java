ca56b56c390df1c20b89096138e5fe4ae5631c1a
klikli19 <103356373+klikli19@users.noreply.github.com>
2023-01-26 19:08:57 +0300

    дз6

---
+++ src/test/java/ru/hogwarts/school/StudentControllerTests.java
@@ -0,0 +1,7 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class StudentControllerTests {
+}
