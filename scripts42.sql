ba03ba5b272c268922d1624a3ebbfb1d34024f73
klikli19 <103356373+klikli19@users.noreply.github.com>
2023-02-02 23:23:47 +0300

    сделаны задание 1,2

--- scripts42.sql
+++ scripts42.sql
@@ -1,8 +1,9 @@
 ALTER TABLE student
     ADD CONSTRAINT age CHECK ( age>=16 ),
     ADD CONSTRAINT name_unique UNIQUE (name),
-    ALTER COLUMN age SET DEFAULT 20,
-    ALTER COLUMN name SET NOT NULL;
+    ADD CONSTRAINT name CHECK ( length(name) >=2 AND  length(name) <= 100),
+    ALTER COLUMN age SET DEFAULT 20;
+
 
 ALTER TABLE faculty
     ADD CONSTRAINT name_color_unique UNIQUE (name, color);
