0fb912cb6961522c10767bd6caa759d430f2d4a3
klikli19 <103356373+klikli19@users.noreply.github.com>
2023-02-12 11:06:19 +0300

    correct

--- src/main/java/ru/hogwarts/school/service/InfoPortServiceTest.java
+++ src/main/java/ru/hogwarts/school/service/InfoPortServiceTest.java
@@ -1,14 +1,18 @@
 package ru.hogwarts.school.service;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile("!production")
+@Profile("test")
 public class InfoPortServiceTest implements InfoPortService{
+
+    @Value("${server.port}")
+    private Integer port;
     @Override
     public Integer getPort() {
-        return 8081;
+        return port;
     }
 }
